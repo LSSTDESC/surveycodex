027075ec6ce5173aa269a0e53847763e79fae8e4
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2021-07-28 21:09:18 +0200

    Add setup and requirements

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='galcheat',
+    version='0.0.1dev',
+    description="Tiny package containing useful parameters from main galaxy surveys",
+    long_description="Tiny package containing useful parameters from main galaxy surveys",
+    author='BlendingToolKit developers',
+    author_email='aboucaud@apc.in2p3.fr',
+    packages=['galcheat'],
+    license='MIT'
+)
