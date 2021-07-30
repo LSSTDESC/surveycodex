46094510580ceefd5a01e922b460bb2da4a3b88b
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2021-07-30 20:54:23 +0200

    Fixing code

--- galcheat/__init__.py
+++ galcheat/__init__.py
@@ -56,10 +56,8 @@ from pathlib import Path
 
 from galcheat.survey import Survey
 
-
 _BASEDIR = Path(__file__).parent.resolve()
 
 survey_info = {
-    path.stem: Survey.from_yaml(path)
-    for path in _BASEDIR.glob("data/*.yaml")
+    path.stem: Survey.from_yaml(path) for path in _BASEDIR.glob("data/*.yaml")
 }
