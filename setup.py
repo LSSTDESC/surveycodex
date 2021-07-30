67509875ebd42eb052383f28022dcc215725e776
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2021-07-30 17:39:15 +0200

    Include data in project setup

--- setup.py
+++ setup.py
@@ -5,11 +5,22 @@ from setuptools import setup
 
 setup(
     name='galcheat',
-    version='0.0.1dev',
-    description="Tiny package containing useful parameters from main galaxy surveys",
-    long_description="Tiny package containing useful parameters from main galaxy surveys",
+    version='0.0.2',
+    description="Tiny library of galaxy surveys most useful parameters (with units)",
+    long_description="Tiny library of galaxy surveys most useful parameters (with units)",
     author='BlendingToolKit developers',
     author_email='aboucaud@apc.in2p3.fr',
+    maintainer='Alexandre Boucaud',
     packages=['galcheat'],
-    license='MIT'
+    package_dir={'galcheat': 'galcheat'},
+    package_data={'galcheat': ['data/*.yaml']},
+    include_package_data=True,
+    install_requires=['astropy', 'pyyaml'],
+    python_requires='>=3.6',
+    license='MIT',
+    project_urls={
+        # 'Documentation':
+        'Source': 'https://github.com/aboucaud/galcheat/',
+        'Tracker': 'https://github.com/aboucaud/galcheat/issues',
+    },
 )
