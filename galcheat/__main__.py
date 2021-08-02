4608028dd52e68e38646a69f1ad340d0c814cb67
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2021-08-02 17:29:05 +0200

    Make sure __main__ runs on pip install

--- galcheat/__main__.py
+++ galcheat/__main__.py
@@ -1,9 +1,15 @@
 from galcheat import survey_info
 
-for survey, info in survey_info.items():
-    print(survey, ":")
-    print("  ", info)
-    print("   Filters :")
-    for filtinfo in info.get_filters():
-        print("     ", filtinfo)
-    print()
+
+def main():
+    for survey, info in survey_info.items():
+        print(survey, ":")
+        print("  ", info)
+        print("   Filters :")
+        for filtinfo in info.get_filters():
+            print("     ", filtinfo)
+        print()
+
+
+if __name__ == "__main__":
+    main()
