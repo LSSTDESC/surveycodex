b3e4b176cfe76beecfd9109e95f3dac4dbe0b84d
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2021-10-10 18:02:13 +0200

    Tentative implementation of functions

---
+++ galcheat/utilities.py
@@ -0,0 +1,71 @@
+import astropy.units as u
+
+from galcheat.helpers import get_filter, get_survey
+
+
+def mag2counts(magnitude, survey_name, filter_name):
+    """Convert source magnitude to counts for a given filter of a survey
+
+    The calculation includes the effects of atmospheric extinction as
+    well as exposure time.
+
+    Parameters
+    ----------
+    magnitude: float
+        magnitude of source
+    survey_name: str
+        Name of a given survey
+    filter_name: str
+        Name of the survey filter
+
+    Returns
+    -------
+    The corresponding flux in counts
+
+    References
+    ----------
+    The `WeakLensingDeblending` package
+    https://github.com/LSSTDESC/WeakLensingDeblending
+
+    """
+    if not isinstance(magnitude, u.Quantity):
+        magnitude *= u.mag(u.ct / u.s)
+    else:
+        magnitude = magnitude.value * u.mag(u.ct / u.s)
+
+    survey = get_survey(survey_name)
+    filter = get_filter(filter_name, survey_name)
+
+    delta_airmass = survey.airmass - survey.zeropoint_airmass
+    mag = magnitude + filter.extinction * delta_airmass
+
+    flux = (mag - filter.zeropoint).to(u.ct / u.s)
+    counts = flux * filter.exposure_time
+
+    return counts
+
+
+def mean_sky_level(survey_name, filter_name):
+    """Computes the mean sky level for a given survey and a filter
+
+    Parameters
+    ----------
+    survey_name: str
+        Name of a given survey
+    filter_name: str
+        Name of the survey filter
+
+    Returns
+    -------
+    The corresponding mean sky level in counts
+
+    """
+    survey = get_survey(survey_name)
+    filter = get_filter(filter_name, survey_name)
+
+    sky_brightness_counts = mag2counts(filter.sky_brightness, survey_name, filter_name)
+    pixel_area = survey.pixel_scale.to_value(u.arcsec) ** 2
+
+    mean_sky_level = sky_brightness_counts * pixel_area
+
+    return mean_sky_level
