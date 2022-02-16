cb91debf545c36133d8905d7fade8fae4175a2fd
mpaillassa <mpaillassa@gmail.com>
2022-02-16 16:40:01 +0900

    separating loops

--- scripts/zeropoints.py
+++ scripts/zeropoints.py
@@ -22,37 +22,37 @@ def calculate_zero_point(band_name, reference_mag=24):
     return filt_units
 
 
-def check_zeropoints():
+def check_zeropoints(survey_name):
     """Recompute the zeropoints with speclite and the effective area
     and compare them to their current values
     """
 
-    for survey_name in available_surveys:
-        if survey_name in speclite_survey_prefixes.keys():
-            survey = get_survey(survey_name)
-            print(survey_name, ":")
+    if survey_name in speclite_survey_prefixes.keys():
+        survey = get_survey(survey_name)
+        print(survey_name, ":")
 
-            speclite_prefix = speclite_survey_prefixes[survey_name]
+        speclite_prefix = speclite_survey_prefixes[survey_name]
 
-            survey_filters = survey.get_filters()
-            for filt_name in survey_filters:
-                speclite_filt_name = f"{speclite_prefix}{filt_name}"
-                zp_24 = (
-                    calculate_zero_point(speclite_filt_name)
-                    * survey.effective_area
-                    * 1
-                    * u.s
-                )
-                zp_btk = (math.log10(zp_24) + 0.4 * 24) / 0.4 * u.mag
+        survey_filters = survey.get_filters()
+        for filt_name in survey_filters:
+            speclite_filt_name = f"{speclite_prefix}{filt_name}"
+            zp_24 = (
+                calculate_zero_point(speclite_filt_name)
+                * survey.effective_area
+                * 1
+                * u.s
+            )
+            zp_btk = (math.log10(zp_24) + 0.4 * 24) / 0.4 * u.mag
 
-                filt = survey.get_filter(filt_name)
-                current_zp = filt.zeropoint
-                print(f"  Filter {filt_name} ({speclite_filt_name} in speclite)")
-                print(f"    Computed value: {zp_btk:.2f}")
-                print(f"    Current value: {current_zp:.2f}")
-        else:
-            print(survey_name, ": filters are not available in speclite")
+            filt = survey.get_filter(filt_name)
+            current_zp = filt.zeropoint
+            print(f"  Filter {filt_name} ({speclite_filt_name} in speclite)")
+            print(f"    Computed value: {zp_btk:.2f}")
+            print(f"    Current value: {current_zp:.2f}")
+    else:
+        print(survey_name, ": filters are not available in speclite")
 
 
 if __name__ == "__main__":
-    check_zeropoints()
+    for survey_name in available_surveys:
+        check_zeropoints(survey_name)
