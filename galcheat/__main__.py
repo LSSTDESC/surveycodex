afd0964e32d2fb60acdef147505234a1a6f803a9
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2022-03-10 19:38:23 +0100

    Use argparse instead of argv for help

--- galcheat/__main__.py
+++ galcheat/__main__.py
@@ -1,28 +1,33 @@
+import argparse
+
 from galcheat.helpers import available_surveys, get_survey, print_survey
 
 _FOOTER = "\nprovided by galcheat <https://github.com/aboucaud/galcheat>\n"
 
 
-def main(survey_name=None):
-    if survey_name is not None:
+def main():
+    parser = argparse.ArgumentParser(
+        description="Print out the main survey features present in galcheat",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-s", type=str, default=None, metavar="survey_name", help="name of survey"
+    )
+    args = parser.parse_args()
+
+    if args.survey_name is None:
+        for sname in available_surveys:
+            survey = get_survey(sname)
+            print_survey(survey)
+            print("\n", "•  " * 15, "\n")
+    else:
         try:
-            survey = get_survey(survey_name)
+            survey = get_survey(args.survey_name)
             print_survey(survey)
         except ValueError as e:
             print(e)
-            pass
-    else:
-        for survey_name in available_surveys:
-            survey = get_survey(survey_name)
-            print_survey(survey)
-            print("\n", "•  " * 15, "\n")
     print(_FOOTER)
 
 
 if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) > 1:
-        main(survey_name=sys.argv[1])
-    else:
-        main()
+    main()
