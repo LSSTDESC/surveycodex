135c7b87a25c6313c259a73b4e843d318b704d00
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2022-03-10 15:51:33 +0100

    Improve main() function

--- galcheat/__main__.py
+++ galcheat/__main__.py
@@ -1,18 +1,28 @@
-from galcheat.helpers import available_surveys, get_survey
+from galcheat.helpers import available_surveys, get_survey, print_survey
+
+_FOOTER = "\nprovided by galcheat <https://github.com/aboucaud/galcheat>\n"
 
 
-def main():
-    for survey_name in available_surveys:
-        survey = get_survey(survey_name)
-        print(survey_name, ":")
-        print("  ", survey)
-
-        survey_filters = survey.get_filters()
-        print("   Filters :")
-        for info in survey_filters.values():
-            print("     ", info)
-        print()
+def main(survey_name=None):
+    if survey_name is not None:
+        try:
+            survey = get_survey(survey_name)
+            print_survey(survey)
+        except ValueError as e:
+            print(e)
+            pass
+    else:
+        for survey_name in available_surveys:
+            survey = get_survey(survey_name)
+            print_survey(survey)
+            print("\n", "•  " * 15, "\n")
+    print(_FOOTER)
 
 
 if __name__ == "__main__":
-    main()
+    import sys
+
+    if len(sys.argv) > 1:
+        main(survey_name=sys.argv[1])
+    else:
+        main()
