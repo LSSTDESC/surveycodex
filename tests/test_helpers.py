9ca0827db084fa306189c1eaaa083c796d2cc41d
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2022-03-21 11:48:00 +0100

    Add copy for the filters

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -11,7 +11,7 @@ def test_survey_copy_not_view():
     assert survey2.obscuration == original_obscuration
 
 
-def test_filter_copy_not_view():
+def test_surveyfilter_copy_not_view():
     survey = get_survey("LSST")
     filter_u = survey.get_filter("u")
     original_zeropoint = filter_u.zeropoint
@@ -19,4 +19,15 @@ def test_filter_copy_not_view():
     filter_u.zeropoint = new_zeropoint
     assert filter_u.zeropoint == new_zeropoint
     filter_u2 = get_survey("LSST").get_filter("u")
+    assert filter_u2.zeropoint == original_zeropoint
+
+
+def test_filter_copy_not_view():
+    survey = get_survey("LSST")
+    filter_u = survey.get_filter("u")
+    original_zeropoint = filter_u.zeropoint
+    new_zeropoint = 42.0 * original_zeropoint.unit
+    filter_u.zeropoint = new_zeropoint
+    assert filter_u.zeropoint == new_zeropoint
+    filter_u2 = survey.get_filter("u")
     assert filter_u2.zeropoint == original_zeropoint
