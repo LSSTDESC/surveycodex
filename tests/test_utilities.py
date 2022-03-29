76e28e673fda285cbe79072fb5436ac9b5b2dee7
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2022-03-29 05:12:02 -0400

    add references for LSST (#101)
    * add references for LSST
    
    * update with v5 arxiv paper and values
    
    * forgot a reference to PDF
    
    * dupate mean sky level for v5
    
    * use WLD directly, increase tolerance
    
    * remove cast to int in mean_sky_level tests

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,24 +1,25 @@
+import numpy as np
 import pytest
 
 from galcheat.helpers import get_survey
 from galcheat.utilities import mag2counts, mean_sky_level
 
-BTK_COUNTS_MAG24 = {
-    "LSST_u": 15321.782101179268,
-    "LSST_g": 121397.91888074754,
-    "LSST_r": 240956.73939657194,
-    "LSST_i": 179448.18815675998,
-    "LSST_z": 108953.51289042353,
-    "LSST_y": 50727.240442255556,
+WLD_COUNTS_MAG24 = {
+    "LSST_u": 15388.800000000001,
+    "LSST_g": 121680.0,
+    "LSST_r": 241224.00000000003,
+    "LSST_i": 178627.19999999998,
+    "LSST_z": 108864.0,
+    "LSST_y": 50784.0,
 }
 
-BTK_MEAN_SKY_LEVEL = {
-    "LSST_u": 1687.9876819744386,
-    "LSST_g": 23241.878848667337,
-    "LSST_r": 127057.1381640446,
-    "LSST_i": 180301.3875959776,
-    "LSST_z": 250784.8105213134,
-    "LSST_y": 293292.6831817095,
+WLD_SKY_LEVEL = {
+    "LSST_u": 1695.370986797218,
+    "LSST_g": 23295.88385352744,
+    "LSST_r": 127198.06540061244,
+    "LSST_i": 179476.4959914193,
+    "LSST_z": 250578.77335308882,
+    "LSST_y": 293620.8532702446,
 }
 
 
@@ -27,7 +28,7 @@ BTK_MEAN_SKY_LEVEL = {
 )
 def lsst_btk_counts(request):
     survey, filt = request.param
-    expected = int(BTK_COUNTS_MAG24[f"{survey}_{filt}"])
+    expected = int(WLD_COUNTS_MAG24[f"{survey}_{filt}"])
     return survey, filt, expected
 
 
@@ -36,39 +37,43 @@ def lsst_btk_counts(request):
 )
 def lsst_btk_sky_level(request):
     survey, filt = request.param
-    expected = int(BTK_MEAN_SKY_LEVEL[f"{survey}_{filt}"])
+    expected = WLD_SKY_LEVEL[f"{survey}_{filt}"]
     return survey, filt, expected
 
 
 def test_mag2counts_str(lsst_btk_counts):
     survey, filt, expected = lsst_btk_counts
-    assert mag2counts(24, survey, filt).value == expected
+    np.testing.assert_allclose(mag2counts(24, survey, filt).value, expected, rtol=1e-1)
 
 
 def test_mag2counts_filter_str(lsst_btk_counts):
     survey, filt, expected = lsst_btk_counts
     survey_inst = get_survey(survey)
-    assert mag2counts(24, survey_inst, filt).value == expected
+    np.testing.assert_allclose(
+        mag2counts(24, survey_inst, filt).value, expected, rtol=1e-1
+    )
 
 
 def test_mag2counts_filter_instance(lsst_btk_counts):
     survey, filt, expected = lsst_btk_counts
     survey_inst = get_survey(survey)
     filt_inst = survey_inst.get_filter(filt)
-    assert mag2counts(24, survey_inst, filt_inst).value == expected
+    np.testing.assert_allclose(
+        mag2counts(24, survey, filt_inst).value, expected, rtol=1e-1
+    )
 
 
 def test_mean_sky_level_str(lsst_btk_sky_level):
     survey, filt, expected = lsst_btk_sky_level
     sky_level = mean_sky_level(survey, filt).value
-    assert int(sky_level) == expected
+    np.testing.assert_allclose(sky_level, expected, rtol=1e-1)
 
 
 def test_mean_sky_level_filter_str(lsst_btk_sky_level):
     survey, filt, expected = lsst_btk_sky_level
     survey_inst = get_survey(survey)
     sky_level = mean_sky_level(survey_inst, filt).value
-    assert int(sky_level) == expected
+    np.testing.assert_allclose(sky_level, expected, rtol=1e-1)
 
 
 def test_mean_sky_level_filter_instance(lsst_btk_sky_level):
@@ -76,4 +81,4 @@ def test_mean_sky_level_filter_instance(lsst_btk_sky_level):
     survey_inst = get_survey(survey)
     filt_inst = survey_inst.get_filter(filt)
     sky_level = mean_sky_level(survey_inst, filt_inst).value
-    assert int(sky_level) == expected
+    np.testing.assert_allclose(sky_level, expected, rtol=1e-1)
