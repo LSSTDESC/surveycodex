a7f8b7f2a603b847c1bd2bfc7905bbd1a8e342a4
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2022-04-02 01:15:17 +0200

    Simplify zeropoint computation

--- scripts/check_zeropoints.py
+++ scripts/check_zeropoints.py
@@ -1,7 +1,7 @@
 import math
 
 from astropy import units as u
-from speclite.filters import ab_reference_flux, load_filter
+from speclite.filters import load_filter
 
 import galcheat
 
@@ -13,13 +13,14 @@ SPECLITE_SURVEY_PREFIXES = {
 }
 
 
-def calculate_zero_point(band_name, reference_mag=24):
+def compute_zeropoint_mag(band_name, effective_area, exposure_time=1 * u.s):
     """Compute the zeropoint of a given filter with speclite"""
     speclite_filter = load_filter(band_name)
-    filter_conv = speclite_filter.convolve_with_function(ab_reference_flux)
-    filter_scaled = filter_conv * 10 ** (-0.4 * reference_mag)
-    filter_units = filter_scaled.to(1 / (u.s * u.m**2))
-    return filter_units
+    zeropoint_counts = (
+        speclite_filter.ab_zeropoint * effective_area * exposure_time
+    ).decompose()
+    zeropoint_mag = 2.5 * math.log10(zeropoint_counts) * u.mag
+    return zeropoint_mag
 
 
 def check_zeropoints(survey_name):
@@ -42,12 +43,9 @@ def check_zeropoints(survey_name):
 
         for filter_name in survey.available_filters:
             speclite_filter_name = f"{speclite_prefix}-{filter_name}"
-            zp_24 = (
-                calculate_zero_point(speclite_filter_name)
-                * survey.effective_area
-                * (1 * u.s)
+            speclite_zp = compute_zeropoint_mag(
+                speclite_filter_name, survey.effective_area
             )
-            speclite_zp = (math.log10(zp_24) + 0.4 * 24) / 0.4 * u.mag
 
             galcheat_filter = survey.get_filter(filter_name)
             current_zp = galcheat_filter.zeropoint
